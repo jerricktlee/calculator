2a9bd8d52e5130e9b334d925a8546e01945b4acf
jerricktlee <83623094+jerricktlee@users.noreply.github.com>
2024-01-23 18:43:59 -0800

    Implement baseline operate() function

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,28 @@
+let operand1;
+let operand2;
+let operator;
+
+function operate(operator, operand1, operand2) {
+   switch (operator) {
+      case '+':
+         return add(operand1, operand2); 
+         break;
+      case '-':
+         return subract(operand1, operand2);
+         break;
+      case '*': 
+         return multiply(operand1, operand2);
+         break;
+      case '/':
+         return divide(operand1, operand2); 
+         break;
+      default:
+         return;
+         break;
+   }
+}
+
+// All basic math operators
 function add(operand1, operand2) {
    return operand1 + operand2;
 }
