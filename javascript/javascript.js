0d847cc687aefd65402ea7be2a8457bf36b6fe65
jerricktlee <83623094+jerricktlee@users.noreply.github.com>
2024-01-24 11:28:14 -0800

    Add event listeners to populate the display

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,25 @@
 let operand1;
 let operand2;
 let operator;
+let displayVal = '0';
+
+const displayText = document.querySelector('#display-text'); 
+const numberBtns = document.querySelectorAll('.number-btn');
+
+numberBtns.forEach((button) => {
+   button.addEventListener('click', () => {
+      let numVal = button.textContent;
+      if (displayVal == '0') {
+         displayVal = numVal;
+         displayText.textContent = displayVal;
+      }
+      else {
+         displayVal += numVal;
+         displayText.textContent = displayVal;
+      }
+
+   });
+})
 
 function operate(operator, operand1, operand2) {
    switch (operator) {
